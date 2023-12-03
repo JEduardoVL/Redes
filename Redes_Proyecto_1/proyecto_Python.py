4720e36a796149d2c6b1109ed7d4065248cc023a
Eduardo_vl <eduardovl_27@outlook.com>
2023-12-02 18:44:12 -0600

    Impresion

--- Redes_Proyecto_1/proyecto_Python.py
+++ Redes_Proyecto_1/proyecto_Python.py
@@ -1,13 +1,14 @@
+'''
 from os import system
 import ipaddress
 
 
-print(''' 
+print(''
       Proyecto Calculadora de redes. 
       Ingresa la IP que desea conocer.
       
       CLase     SubRedes       Host
-      ''')
+      '')
 
 ingreso = input()
 
@@ -29,7 +30,7 @@ def ip_calculator(ip_cidr):
     if ip_class in ['A', 'B', 'C']:
         mask = network.prefixlen
         base_masks = {'A': 8, 'B': 16, 'C': 24}
-        subnet_count = 2 ** (mask - base_masks[ip_class])
+        subnet_count = 2 ** (mask - base_masks[ip_class])    
     else:
         subnet_count = 'No aplicable'
 
@@ -38,3 +39,43 @@ def ip_calculator(ip_cidr):
     return f"Clase = {ip_class}, SubRedes = {subnet_count}, Hosts = {host_count}"
 
 print(ip_calculator(ingreso))
+'''
+
+from ipaddress import ip_network
+
+print(''' 
+      Proyecto Calculadora de redes. 
+      Ingresa la IP que desea conocer.
+      
+      ''')
+
+ingreso = input()
+
+def ip_calculator(ip_cidr):
+    network = ip_network(ip_cidr, strict=False)
+    netmask = network.netmask
+    wildcard = network.hostmask
+    network_address = network.network_address
+    broadcast_address = network.broadcast_address
+    host_min = list(network.hosts())[0]
+    host_max = list(network.hosts())[-1]
+    num_hosts = network.num_addresses - 2  # Excluye la dirección de red y broadcast
+
+    # Convertir a formato binario
+    def to_binary(ip):
+        return '.'.join([format(int(octet), '08b') for octet in str(ip).split('.')])
+
+    return (
+        f"Address:    \t{network_address}\t{to_binary(network_address)}\n"
+        f"Netmask:    \t{netmask} = {network.prefixlen}\t{to_binary(netmask)}\n"
+        f"Wildcard:   \t{wildcard}\t{to_binary(wildcard)}\n"
+        "=>\n"
+        f"Network:    \t{network_address}/{network.prefixlen}\t{to_binary(network_address)}\n"
+        f"HostMin:    \t{host_min}\t{to_binary(host_min)}\n"
+        f"HostMax:    \t{host_max}\t{to_binary(host_max)}\n"
+        f"Broadcast:  \t{broadcast_address}\t{to_binary(broadcast_address)}\n"
+        f"Hosts/Net:  \t{num_hosts}\tClass {network_address.max_prefixlen - network.prefixlen}"
+    )
+
+print(ip_calculator(ingreso))
+
